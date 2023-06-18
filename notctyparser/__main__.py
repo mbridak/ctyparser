a17409590900dc369f77c3e953981377c61ba537
mbridak <michael.bridak@gmail.com>
2023-06-18 07:26:27 -0700

    tests

--- notctyparser/__main__.py
+++ notctyparser/__main__.py
@@ -7,7 +7,7 @@ Released under the terms of the MIT license.
 """
 
 
-# import argparse
+import os
 import pathlib
 
 import notctyparser
@@ -17,6 +17,10 @@ try:
     cty = notctyparser.BigCty(file)
 except FileNotFoundError:
     cty = notctyparser.BigCty()
+
+print(f"LANG: {os.environ.get('LANG')}")
+print(f"LC_TIME: {os.environ.get('LC_TIME')}")
+print("Update:", cty.check_update())
 print("Updated:", cty.update())
 print("Datestamp:", cty.formatted_version)
 print("Version Entity:", cty.get("VERSION", "Not present, data possibly corrupted."))
